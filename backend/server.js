269e76bfbcd50d378cf77fb7a8de7bff95ace47b
Shivangi1515 <shivangichaurasia1515@gmail.com>
2026-07-22 03:42:51 +0530

    feat(backend): add server entrypoint, database connection, and API middleware

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api",chatRoutes);
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
+    connectDB();
+});
+
+const connectDB= async() =>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("connected with DB");
+
+    }catch(err){
+        console.log("Failed to connect to db",err);
+
+    }
+}
+
+
+
+
+
