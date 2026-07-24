0dec18ecdd0078a45454e29ac5b67e0ef2e0e30d
Shivangi1515 <shivangichaurasia1515@gmail.com>
2026-07-24 23:18:14 +0530

    fix(backend): remove next callback from async pre-save Hook in User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,15 +24,10 @@ const UserSchema = new mongoose.Schema({
 });
 
 // Pre-save hook to hash password
-UserSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
-    try {
-        const salt = await bcryptjs.genSalt(10);
-        this.password = await bcryptjs.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+UserSchema.pre("save", async function() {
+    if (!this.isModified("password")) return;
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
 });
 
 // Instance method to compare password
